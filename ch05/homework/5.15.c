5816ff62221d6099ad3454bda16babf6d8d6d187
ZHKO1 <zhko11993@gmail.com>
2023-05-14 20:40:58 +0800

    Ch05 update

--- ch05/homework/5.15.c
+++ ch05/homework/5.15.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include "./lib/vec.h"
 
-void inner5(vec_ptr u, vec_ptr v, data_t *dest)
+void inner6(vec_ptr u, vec_ptr v, data_t *dest)
 {
   long i;
   long k = 6;
@@ -11,23 +11,28 @@ void inner5(vec_ptr u, vec_ptr v, data_t *dest)
   long limit = length - k + 1;
   data_t *udata = get_vec_start(u);
   data_t *vdata = get_vec_start(v);
-  data_t sum = (data_t)0;
+  data_t sum0 = (data_t)0;
+  data_t sum1 = (data_t)0;
+  data_t sum2 = (data_t)0;
+  data_t sum3 = (data_t)0;
+  data_t sum4 = (data_t)0;
+  data_t sum5 = (data_t)0;
 
   for (i = 0; i < limit; i += k)
   {
-    sum = sum + udata[i] * vdata[i];
-    sum = sum + udata[i + 1] * vdata[i + 1];
-    sum = sum + udata[i + 2] * vdata[i + 2];
-    sum = sum + udata[i + 3] * vdata[i + 3];
-    sum = sum + udata[i + 4] * vdata[i + 4];
-    sum = sum + udata[i + 5] * vdata[i + 5];
+    sum0 += udata[i] * vdata[i];
+    sum1 = udata[i + 1] * vdata[i + 1];
+    sum2 = udata[i + 2] * vdata[i + 2];
+    sum3 = udata[i + 3] * vdata[i + 3];
+    sum4 = udata[i + 4] * vdata[i + 4];
+    sum5 = udata[i + 5] * vdata[i + 5];
   }
 
   for (; i < length; i++)
   {
-    sum = sum + udata[i] * vdata[i];
+    sum0 += udata[i] * vdata[i];
   }
-  *dest = sum;
+  *dest = sum0 + sum1 + sum2 + sum3 + sum4 + sum5;
 }
 
 int main(int argc, char *argv[])
@@ -52,7 +57,8 @@ int main(int argc, char *argv[])
   set_vec_start(v, arr);
 
   data_t res;
-  inner5(u, v, &res);
+  inner6(u, v, &res);
+
   assert(res == 1 + 4 + 9 + 16 + 25 + 36 + 49 + 64 + 81 + 100);
   return 0;
 }
