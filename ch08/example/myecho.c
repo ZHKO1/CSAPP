b5454bfea49ffa89c4be191fafe0e0e1b720b809
ZHKO1 <zhko11993@gmail.com>
2023-06-19 15:03:40 +0800

    Ch08 update

---
+++ ch08/example/myecho.c
@@ -0,0 +1,19 @@
+#include "./lib/csapp.h"
+
+int main(int argc, char *argv[], char *envp[])
+{
+  int i = 0;
+  printf("Command-ine arguments\n");
+  for (i = 0; i < argc; i++)
+  {
+    printf("arg[%d]: %s\n", i, argv[i]);
+  }
+  printf("Environment variables\n");
+  i = 0;
+  char *env = NULL;
+  while (env = envp[i++])
+  {
+    printf("envp[%d]: %s\n", i, env);
+  };
+  return 0;
+}
